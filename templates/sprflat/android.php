adba4ba2881a6355586a8538f36fc1ec620438a3
cuongnd <nguyendinhcuong@gmail.com>
2016-06-07 18:28:35 +0700

    dfd

--- templates/sprflat/android.php
+++ templates/sprflat/android.php
@@ -6,13 +6,11 @@ $active_menu=$menu->getActive();
 $mobile_response_type=$active_menu->mobile_response_type;
 $doc=JFactory::getDocument();
 $component =$doc->getBuffer('component');
-require_once JPATH_ROOT.'/libraries/simplehtmldom_1_5/simple_html_dom.php';
 if($active_menu->mobile_response_type=='json'){
-	$html = str_get_html($component);
-    echo "<pre>";
-    echo print_r($html);
-    echo "</pre>";
-    die;
+    $dom = new DOMDocument();
+    $dom->loadHTML($component);
+    $json_html= JUtility::element_to_obj($dom->documentElement);
+    echo json_encode($json_html);
 }else{
 	echo $component;
 }
