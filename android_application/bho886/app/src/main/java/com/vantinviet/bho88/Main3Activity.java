9378f260a0d3de2b4f9152b3e1b79af905d8377e
cuongnd <nguyendinhcuong@gmail.com>
2016-06-23 09:19:06 +0700

    dfdfd

---
+++ android_application/bho886/app/src/main/java/com/vantinviet/bho88/Main3Activity.java
@@ -0,0 +1,39 @@
+package com.vantinviet.bho88;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.entity.scene.Scene;
+import org.andengine.ui.activity.LayoutGameActivity;
+
+public class Main3Activity extends LayoutGameActivity {
+
+
+    @Override
+    protected int getLayoutID() {
+        return 0;
+    }
+
+    @Override
+    protected int getRenderSurfaceViewID() {
+        return 0;
+    }
+
+    @Override
+    public EngineOptions onCreateEngineOptions() {
+        return null;
+    }
+
+    @Override
+    public void onCreateResources(OnCreateResourcesCallback pOnCreateResourcesCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onCreateScene(OnCreateSceneCallback pOnCreateSceneCallback) throws Exception {
+
+    }
+
+    @Override
+    public void onPopulateScene(Scene pScene, OnPopulateSceneCallback pOnPopulateSceneCallback) throws Exception {
+
+    }
+}
